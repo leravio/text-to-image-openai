42d2a5fdd4bd91fd6bc0bfbd61e453b63a664a49
varreltantio <snetid90@gmail.com>
2023-02-21 21:57:29 +0700

    update url axios

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError("");
 
     await axios
-      .post(`http://localhost:5000`, {
+      .post(process.env.REACT_APP_BACKEND_URL, {
         prompt,
       })
       .then((response) => setData(response.data.image_url))
--- server/index.js
+++ server/index.js
@@ -45,5 +45,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
-export default app;
