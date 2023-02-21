d3a1efd35d067bcbd429fcd08ce379b5815a6ac8
varreltantio <snetid90@gmail.com>
2023-02-21 20:13:11 +0700

    add port in env

--- server/server.js
+++ server/server.js
@@ -40,6 +40,8 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("AI server started on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
